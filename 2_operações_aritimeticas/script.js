a6babba7fc44a6d9c46b2769307ed136ad84bdb6
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-12 14:58:51 -0300

    Adicionando String

--- 2_operações_aritimeticas/script.js
+++ 2_operações_aritimeticas/script.js
@@ -9,4 +9,4 @@ console.log (10 / 10)
 console.log (10 % 2)
 console.log (11 % 2)
 
-console.log (typeof 10)
+console.log (typeof (11 % 2))
---
+++ 3_string/script.js
@@ -0,0 +1,11 @@
+console.log ("Teste de string" + " 1");
+console.log ("Teste de string 1");
+console.log ("Teste de string 3");
+console.log ("Teste de string 4");
+
+console.log ("Infinity");
+console.log (Infinity);
+
+console.log (typeof "Teste de string 4");
+console.log (typeof "Infinity");
+console.log (typeof Infinity);

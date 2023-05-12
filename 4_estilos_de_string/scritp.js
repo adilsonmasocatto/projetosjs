66ea922f351d0115e48b8f353aa03850be97282e
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-12 15:45:30 -0300

    Adicionando estilos de String

---
+++ 4_estilos_de_string/scritp.js
@@ -0,0 +1,6 @@
+console.log ("Primeira linha \nSegunda linha");
+console.log ("Meu nome é 'Adilson");
+console.log ('Meu nome é "Adilson"');
+console.log (`02 + 02 é = ${ 2 + 2}`);
+console.log(`50 vezes 1 é = ${50*1}`);
+console.log ("Teste de string" + " 1");

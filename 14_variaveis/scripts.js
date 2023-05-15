f000ee338fcd4f543efc3ee4db2e8f7bb0638f86
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-15 16:43:20 -0300

    Variaveis Const e Var

---
+++ 14_variaveis/scripts.js
@@ -0,0 +1,26 @@
+let nome = "Adilson Masocatto Junior";
+console.log (nome);
+console.log (`O meu nome é: ${nome}`);
+
+nome = "João";
+console.log(nome);
+
+nome = "Maria";
+console.log(nome);
+
+let laranjas = 10;
+let laranja = 1;
+console.log ( laranjas * 5 );
+console.log ( laranja);
+console.log ( laranjas * laranja);
+console.log ( laranjas * laranjas);
+
+laranjas = 6;
+console.log ( laranjas + laranja);
+
+laranja = "Laranja"
+console.log ( laranja )
+
+let um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
+

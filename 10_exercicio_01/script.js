98202d5ee6b42fcb8583835a6e8bb6a10a5e5e5f
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-15 11:58:56 -0300

    Exercicio 01

---
+++ 10_exercicio_01/script.js
@@ -0,0 +1,4 @@
+console.log('Hello World!');
+console.log("Hello World 02!");
+console.log('"Hello World 03"');
+console.log("'Hello' World 04");
---
+++ 9_conversao_automatica/script.js
@@ -0,0 +1,8 @@
+console.log(typeof ("123" + 4));
+console.log("123" + 4);
+console.log( "5" * 5);
+console.log(typeof ("5" * 5));
+console.log( "10" - 20);
+console.log(typeof ("10" - 20));
+console.log( "hauhauha" - 20);
+console.log(typeof ("huahauhau" * 20));

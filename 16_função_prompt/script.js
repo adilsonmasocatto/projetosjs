5b2aa2f392ad39f5991a25d9ea3bf5e37f9b9e66
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-16 15:31:27 -0300

    Teste de Funções

---
+++ 16_função_prompt/script.js
@@ -0,0 +1,10 @@
+let idade = prompt ("Qual a sua idade?");
+
+console.log (idade);
+
+let nome = prompt ("Qual o seu nome");
+
+console.log (`Seu nome é ${nome}`);
+
+console.log (`Seu nome é ${nome} e tenho ${idade} anos!`);
+

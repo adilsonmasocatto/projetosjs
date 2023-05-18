b4e6ae5c4bcaa44040eba06d38fec93f01e435d6
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-18 10:39:56 -0300

    Função Math

---
+++ 18_função_math/scripts.js
@@ -0,0 +1,6 @@
+let maior = Math.max(5,10,15,20,25,30);
+console.log (maior);
+let menor = Math.min(1,2,3,4,5,6,7);
+console.log (menor);
+let arredondar = Math.round (6.50);
+console.log (arredondar);

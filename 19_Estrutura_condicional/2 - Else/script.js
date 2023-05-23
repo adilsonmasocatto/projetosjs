71974c40c21c56df42d9c61de01704aa62123f80
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-23 14:35:57 -0300

    Else IF

---
+++ 19_Estrutura_condicional/2 - Else/script.js
@@ -0,0 +1,15 @@
+let nome = "Joao";
+
+if (nome == 'João') {
+    console.log ('Seu nome é João');
+} else {
+    console.log ('Seu nome não é João');
+}
+
+let velocidade = 100;
+
+if (velocidade >= 100) {
+    console.log ('Você foi multado');
+} else {
+    console.log ('Você não foi multado');
+}
---
+++ 19_Estrutura_condicional/3 - Else IF/script.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 6;
+
+if (a + b == 3){
+    console.log ('O resultado é 3');
+} else if ( a == 4) {
+    console.log ('O valor A é 4');
+} else if ( b == 5) {
+    console.log ( 'O valor B é 5');
+} else {
+    console.log ('Nenhuma das condições acima!');
+}

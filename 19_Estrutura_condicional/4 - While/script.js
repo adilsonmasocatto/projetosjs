9b54a886bc970bb71047dc149e8813ed24817fcb
Adilson Masocatto Junior <adilson.masocatto@levva.io>
2023-05-24 14:48:54 -0300

    While

---
+++ 19_Estrutura_condicional/4 - While/script.js
@@ -0,0 +1,24 @@
+//Estrutura de Repetição
+
+
+let y = 10;
+
+while( y > -1) {
+    console.log ("O Y é " + y);
+    y = y -1;
+}
+
+
+let x = 0;
+
+while( x < 11) {
+    console.log ("O X é " + x);
+    x = x +1;
+}
+
+let z = 0;
+
+while( z <= 15) {
+    console.log ("O Z é " + z);
+    z = z +1;
+}
